55446ff235f8d9b00b87ab2c0a19070557b32e19
TVerloop <t93.verloop@gmail.com>
2018-01-29 11:53:13 +0100

    Added new style (Doom one)

--- custom.el
+++ custom.el
@@ -5,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("a21978b65dbc3221dbb558fb1689e4b6c8c56a74c36cac514b4d0a727c1a52bc" "604b42cb77d48f7930f3786cc81388cdcc1a456ee77ff8fcdceb83e2d8aebd66" "c2df2edbb2f3afd0357501421033e41bd69d8197c130fe23e9e42d38f31fd897" "5a4d82f15485f20329524249df1af360491dda942c9c8e49bf162df75dc41f3e" "85bfe1307fac2c04c377a52b6cbe1ecf1e637e43ab61ecaecbc2c81135e0edcc" "dca901d443a63f1720abec4e40bd5d38099cde8e059417c398cc2bac30f9fdbc" "747c3b81de23fa44894033eb48d959e8e2aac021f246d0693b6e071851ee20e2" "9527feeeec43970b1d725bdc04e97eb2b03b15be982ac50089ad223d3c6f2920" default)))
+    ("b8001c42fbb84ab085bec86ed2dacdd14d3b4524bc781c1bf7fd7374780db7d0" "70bb03d2d7e8130a81f1c89bf50c5b4985685b67b7398c9ffe600333b8bd89f0" "c5754efb23a14abd65ac43b71b8e6148456559a680a27863b333990a53c2c21a" "05c29bc1f3abe7e74b71a843999bdb9c7ab9677160d20256a833b8d33b043966" "8cdbc58e953c48b079aa1d3c1e09e0245334a3c6cae83205a0348f6bb97c5748" "48d66d015a15ce74c0fa0a9cec3c59162e322e853d44375c4e938a713be6e5c7" "a21978b65dbc3221dbb558fb1689e4b6c8c56a74c36cac514b4d0a727c1a52bc" "604b42cb77d48f7930f3786cc81388cdcc1a456ee77ff8fcdceb83e2d8aebd66" "c2df2edbb2f3afd0357501421033e41bd69d8197c130fe23e9e42d38f31fd897" "5a4d82f15485f20329524249df1af360491dda942c9c8e49bf162df75dc41f3e" "85bfe1307fac2c04c377a52b6cbe1ecf1e637e43ab61ecaecbc2c81135e0edcc" "dca901d443a63f1720abec4e40bd5d38099cde8e059417c398cc2bac30f9fdbc" "747c3b81de23fa44894033eb48d959e8e2aac021f246d0693b6e071851ee20e2" "9527feeeec43970b1d725bdc04e97eb2b03b15be982ac50089ad223d3c6f2920" default)))
  '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
  '(package-selected-packages
    (quote
-    (uniquify smooth-scrolling helm-config zygospore use-package company-c-headers company helm-projectile helm-gtags dired helm projectile magit flycheck-pos-tip flycheck bash-completion undo-tree neotree shell-command smartparens neotree))))
+    (switch-buffer-functions uniquify smooth-scrolling helm-config zygospore use-package company-c-headers company helm-projectile helm-gtags dired helm projectile magit flycheck-pos-tip flycheck bash-completion undo-tree neotree shell-command smartparens neotree))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- settings/appearance.el
+++ settings/appearance.el
@@ -12,31 +12,8 @@
 ;; Highlight current line
 (global-hl-line-mode 1)
 
-;; Set custom theme path
-(setq custom-theme-directory (concat user-emacs-directory "themes"))
-
-(dolist
-    (path (directory-files custom-theme-directory t "\\w+"))
-  (when (file-directory-p path)
-    (add-to-list 'custom-theme-load-path path)))
-
-;; Default theme
-(defun use-default-theme ()
-  (interactive)
-  (load-theme 'default-black))
-
-(use-default-theme)
-
 ;; Don't defer screen updates when performing operations
 (setq redisplay-dont-pause t)
-
-;; org-mode colors
-(setq org-todo-keyword-faces
-      '(
-        ("INPR" . (:foreground "yellow" :weight bold))
-        ("DONE" . (:foreground "green" :weight bold))
-        ("IMPEDED" . (:foreground "red" :weight bold))
-        ))
 
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
