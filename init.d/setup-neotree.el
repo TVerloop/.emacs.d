71ed7f39cc02391f6c3e33fccfaa816d174f6b37
TVerloop <t93.verloop@gmail.com>
2018-06-14 16:39:18 +0200

    feat: neotree shows hidden files.

--- init.d/setup-neotree.el
+++ init.d/setup-neotree.el
@@ -12,7 +12,9 @@
   ;; Toggle neotree with f8
   (global-set-key [f8] 'neotree-toggle)
   ;; Use icons.
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)
+        neo-show-hidden-files t
+        neo-show-updir-line t))
 
 (provide 'setup-neotree)
 ;;; setup-neotree ends here
