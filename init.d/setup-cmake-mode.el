3a57fcfc8ff843cae395690e539542e816662d56
TVerloop <t93.verloop@gmail.com>
2018-06-28 09:59:55 +0200

    fix: Comma instead of dot.

--- init.d/setup-cmake-mode.el
+++ init.d/setup-cmake-mode.el
@@ -7,13 +7,16 @@
 (require 'req-package)
 
 (req-package cmake-mode
-  :require cmake-font-lock
+  :require cmake-font-lock company company-cmake
   :config
   ;; Enable CMake Mode when in file "CMakelists.txt" or "*.cmake"
   (setq auto-mode-alist (append
-                         '(("CMakeLists\\.txt\\'" , cmake-mode))
+                         '(("CMakeLists\\.txt\\'" . cmake-mode))
                          '(("\\,cmake\\'" . cmake-mode))
-                         auto-mode-alist)))
+                         auto-mode-alist))
+
+  (add-hook 'cmake-mode-hook (lambda ()
+                               (add-to-list 'company-backends 'company-cmake))))
 
 (provide 'setup-cmake-mode)
 ;;; setup-cmake-mode.el ends here
