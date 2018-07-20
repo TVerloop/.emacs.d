86c27ab1a3b2c0ebb0838c9d6745d8df7a8668e2
TVerloop <t93.verloop@gmail.com>
2018-07-20 16:36:40 +0200

    feat: improved file skeletons.
     * Added additional skeletons.
     * Added file exclusion mechanism.

--- init.d/setup-autoinsert.el
+++ init.d/setup-autoinsert.el
@@ -10,55 +10,180 @@
 ;;; Code:
 
 (require 'autoinsert)
+(require 'helm)
 
-(custom-set-variables
- '(auto-insert-mode t)
- '(auto-insert-query nil)
- '(auto-insert 'other))
+(defvar auto-insert-exclude-file
+  '("custom.el"
+    "ede-projects.el"))
 
-(add-to-list 'auto-insert-alist '(("\\.\\([C]\\|cc\\|cpp\\)\\'" . "C++ source")
-                                  nil
-                                  "/* -*- mode: c++ -*- */\n"
-                                  "/**\n"
-                                  "*  @file    "(file-name-nondirectory (buffer-name))"\n"
-                                  "*  @author  "user-full-name" <"user-mail-address">\n"
-                                  "*  @version 0.1\n"
-                                  "*  @date    "(format-time-string "%d-%m-%Y")"\n"
-                                  "*  @brief   "(helm-read-string "Brief description:")"\n"
-                                  "*/\n"
-                                  "\n"
-                                  "/******************************************************************************/\n"
-                                  "/* Includes.                                                                  */\n"
-                                  "/******************************************************************************/\n"
-                                  "\n"
-                                  "/* System */\n"
-                                  "\n"
-                                  "/* Third party */\n"
-                                  "\n"
-                                  "/* Local */\n"
-                                  "\n"
-                                  "\n"
-                                  "/******************************************************************************/\n"
-                                  "/* Macros.                                                                    */\n"
-                                  "/******************************************************************************/\n"
-                                  "\n"
-                                  "/******************************************************************************/\n"
-                                  "/* Private Variables.                                                         */\n"
-                                  "/******************************************************************************/\n"
-                                  "\n"
-                                  "/******************************************************************************/\n"
-                                  "/* Private function prototypes.                                               */\n"
-                                  "/******************************************************************************/\n"
-                                  "\n"
-                                  "/******************************************************************************/\n"
-                                  "/* function definitions.                                                      */\n"
-                                  "/******************************************************************************/\n"
-                                  > _ "\n"
-                                  "/******************************************************************************/\n"
-                                  "/* EOF.                                                                       */\n"
-                                  "/******************************************************************************/\n"))
+(defun create-from-skeleton ()
+  "Fills new file from skeleton."
+  (if (not (file-exists-p buffer-file-name))
+      (if (not (member (file-name-nondirectory buffer-file-name) auto-insert-exclude-file))
+          (auto-insert))))
 
+(add-hook 'find-file-hook 'create-from-skeleton)
 
+(setq auto-insert-query nil
+      auto-insert 'other)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C++ Skeletons.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-skeleton cpp-source-skeleton
+  "C++ Source file."
+  "/* -*- mode: C++ -*- */\n"
+  "/**\n"
+  " * @file    " (file-name-nondirectory buffer-file-name) "\n"
+  " * @author  " user-full-name " <" user-mail-address ">\n"
+  " * @version 1.0\n"
+  " * @date    " (format-time-string "%d-%m-%Y") "\n"
+  " * \n"
+  " * " (helm-read-string "Brief description: ") "\n"
+  " */\n"
+  "\n"
+  "\n"
+  _ "\n"
+  "\n")
+
+(define-skeleton cpp-header-skeleton
+  "C++ Header file."
+  "/* -*- mode: C++ -*- */\n"
+  "/**\n"
+  " * @file    " (file-name-nondirectory buffer-file-name) "\n"
+  " * @author  " user-full-name " <" user-mail-address ">\n"
+  " * @version 1.0\n"
+  " * @date    " (format-time-string "%d-%m-%Y") "\n"
+  " * \n"
+  " * " (helm-read-string "Brief description: ") "\n"
+  " */\n"
+  "\n"
+  "#ifndef " (upcase (file-name-base buffer-file-name)) "_" (upcase (file-name-extension buffer-file-name)) "__\n"
+  "#define " (upcase (file-name-base buffer-file-name)) "_" (upcase (file-name-extension buffer-file-name)) "__\n"
+  "\n"
+  "\n"
+  _ "\n"
+  "\n"
+  "\n"
+  "#endif // " (upcase (file-name-base buffer-file-name)) "_" (upcase (file-name-extension buffer-file-name)) "__\n"
+  "\n")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C Skeletons.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-skeleton c-source-skeleton
+  "C Source file."
+  "/* -*- mode: C -*- */\n"
+  "/**\n"
+  " * @file    " (file-name-nondirectory buffer-file-name) "\n"
+  " * @author  " user-full-name " <" user-mail-address ">\n"
+  " * @version 1.0\n"
+  " * @date    " (format-time-string "%d-%m-%Y") "\n"
+  " * \n"
+  " * " (helm-read-string "Brief description: ") "\n"
+  " */\n"
+  "\n"
+  "\n"
+  _ "\n"
+  "\n")
+
+(define-skeleton c-header-skeleton
+  "C Header file."
+  "/* -*- mode: C -*- */\n"
+  "/**\n"
+  " * @file    " (file-name-nondirectory buffer-file-name) "\n"
+  " * @author  " user-full-name " <" user-mail-address ">\n"
+  " * @version 1.0\n"
+  " * @date    " (format-time-string "%d-%m-%Y") "\n"
+  " * \n"
+  " * " (helm-read-string "Brief description: ") "\n"
+  " */\n"
+  "\n"
+  "\n"
+  "#ifndef " (upcase (file-name-base buffer-file-name)) "_" (upcase (file-name-extension buffer-file-name)) "__\n"
+  "#define " (upcase (file-name-base buffer-file-name)) "_" (upcase (file-name-extension buffer-file-name)) "__\n"
+  "\n"
+  "#ifdef __cplusplus\n"
+  "extern \"C\" {\n"
+  "#endif\n"
+  "\n"
+  "\n"
+  _ "\n"
+  "\n"
+  "\n"
+  "#ifdef __cplusplus\n"
+  "} // extern \"C\"\n"
+  "#endif\n"
+  "\n"
+  "#endif // " (upcase (file-name-base buffer-file-name)) "_" (upcase (file-name-extension buffer-file-name)) "__\n"
+  "\n")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CMake Skeletons.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-skeleton cmake-list-skeleton
+  "CMakeLists.txt file"
+  "# -*- mode: CMake -*-\n"
+  "#==============================================================================#\n"
+  "# File:     " (file-name-base buffer-file-name) "\n"
+  "# Author:   " user-full-name "   <" user-mail-address ">\n"
+  "# Version:  1.0\n"
+  "# Date:     " (format-time-string "%d-%m-%Y") "\n"
+  "#\n"
+  "# " (helm-read-string "Brief description: ") "\n"
+  "#\n"
+  "#==============================================================================#\n"
+  "cmake_minimum_required(VERSION 3.0)\n"
+  "\n"
+  "project(" (helm-read-string "Project name: ") "\n"
+  > "VERSION 1.0.0)\n"
+  "\n"
+  "\n"
+  _ "\n"
+  "\n")
+
+(define-skeleton cmake-module-skeleton
+  "CMake module file"
+  "# -*- mode: CMake -*-\n"
+  "#==============================================================================#\n"
+  "# File:     " (file-name-base buffer-file-name) "\n"
+  "# Author:   " user-full-name "   <" user-mail-address ">\n"
+  "# Version:  1.0\n"
+  "# Date:     " (format-time-string "%d-%m-%Y") "\n"
+  "#\n"
+  "# " (helm-read-string "Brief description: ") "\n"
+  "#\n"
+  "#==============================================================================#\n"
+  "\n"
+  "\n"
+  _ "\n"
+  "\n")
+
+(define-auto-insert
+  "\\.\\([C]\\|c++\\|cc\\|cpp\\|cxx\\)\\'"
+  'cpp-source-skeleton)
+
+(define-auto-insert
+  "\\.\\(inc\\|hh\\|hpp\\|hxx\\)\\'"
+  'cpp-header-skeleton)
+
+(define-auto-insert
+  "\\.\\(c\\)\\'"
+  'c-source-skeleton)
+
+(define-auto-insert
+  "\\.\\(h\\)\\'"
+  'c-header-skeleton)
+
+(define-auto-insert
+  "CMakeLists\\.txt\\'"
+  'cmake-list-skeleton)
+
+(define-auto-insert "\\.cmake\\'"
+  'cmake-module-skeleton)
 
 (provide 'setup-autoinsert)
 ;;; setup-autoinsert.el ends here
